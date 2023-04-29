f0f999d22a6533a026bfb923d70aac5e0813edf3
sagemooneyham <100988295+sagemooneyham@users.noreply.github.com>
2023-04-29 16:37:21 -0500

    Delete Software/joycontrol directory

--- Software/joycontrol/scripts/mount.sh
+++
@@ -1,1 +0,0 @@
-sshfs -o allow_other raspi:/home/pi/projects/switch/joycontrol/ remote_joycontrol/
--- Software/joycontrol/scripts/restart_bluetooth.sh
+++
@@ -1,5 +0,0 @@
-# realod systemd config changed by reconfigure
-systemctl daemon-reload
-
-# restart bluez with the new settings
-systemctl restart bluetooth.service
