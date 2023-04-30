6571be25c031148a13a1e4a93bab33fa0a6cd133
sagemooneyham <100988295+sagemooneyham@users.noreply.github.com>
2023-04-29 20:52:11 -0500

    Update speedSensor.ino

--- Software/Arduino/speedSensor.ino
+++ Software/Arduino/speedSensor.ino
@@ -26,30 +26,11 @@ void loop() {
       while(!digitalRead(2)){
         
       }
-
-     // counter = 0;
-      /*while(!digitalRead(2) && counter < 10) {
-        counter = counter + 1;
-        delay(100);
-      } //traps until off tape or spent 3 seconds
-      if(counter >= 10) { //spent too much time on tape hit 1, speed is zero
-        applyTime1 = 1;
-        applyTime2 = 0;
-        Serial.println("Been stopped on tape 1");
-        Serial.print("Time for one revolution:");
-        Serial.println(0);
-        Serial1.println(0);
-        counter = 0;
-      }*/
-
     }
     else if(!digitalRead(2) && applyTime2) {
-      
       time2ms = millis();
       applyTime2 = 0;
       applyTime1 = 1;
-      //Serial.print("Hit two time:");
-      //Serial.println(time2ms);
       time1s = time1ms / 1000;
       time2s = time2ms / 1000;
       timeDiff = time2s - time1s;  //time (seconds) between two tape hits
@@ -59,27 +40,5 @@ void loop() {
         Serial.println(revSpeed);
         Serial1.println(revSpeed);
       }
-
-      
-      /*counter = 0;
-      while(!digitalRead(2) && counter < 10) {
-        counter = counter + 1;
-        delay(100);
-      } //traps until off tape or spent 1 seconds
-      if(counter >= 10) { //spent too much time on tape hit 2, speed is zero
-        Serial.println("Been stopped for 1 sec");
-        Serial.print("Time for one revolution:");
-        Serial.println(0);
-        Serial1.println(0);
-        counter = 0;
-      }
     }
-    if(counter >= 10 && digitalRead(2)) { //been stopped for 3 seconds
-      Serial.println("Been stopped for 1 sec");
-      Serial.print("Time for one revolution:");
-      Serial.println(0);
-      Serial1.println(0);
-      counter = 0;
-    }*/
-}
 }
